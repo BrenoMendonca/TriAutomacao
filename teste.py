8572805b58355f7d5e260b982818b0bdec797919
Breno Mendonca <breno_a99@hotmail.com>
2023-08-25 12:10:58 -0300

    Criando o Loop e o tempo de atualização  para vários cadastros

--- teste.py
+++ teste.py
@@ -12,11 +12,6 @@ Arquivo = 'Filiais.xlsx'
 #df = DataFrame
 df = pd.read_excel(Arquivo)
 
-for index,row in df.iterrows():
-    print(' Linha: ' + str(index) + "Nome da filial e  " + row["Nome da Filial"])
-
-
-
 # Atualizando o Chrome Driver Manager automaticamente
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
@@ -29,7 +24,7 @@ navegador = webdriver.Chrome(service=servico)
 
 
 #Acessando o dominio
-navegador.get('https://homologacao.trilogo.app/companies')
+navegador.get('https://gruponatureza.trilogo.app')
 
 time.sleep(2)
 
@@ -41,23 +36,33 @@ navegador.find_element('xpath','//*[@id="email"]').send_keys(login)
 navegador.find_element('xpath','//*[@id="password"]').send_keys(senha)
 navegador.find_element('xpath','//*[@id="submit"]').click()
 
-time.sleep(3)  # Aguarda 10 segundos (pode ajustar esse valor)
+time.sleep(3)  # Aguarda 03 segundos (pode ajustar esse valor)
 
-navegador.get('https://homologacao.trilogo.app/companies')
 
-navegador.find_element('xpath','//*[@id="rc-tabs-0-panel-1"]/div/div/div/div[1]/div[1]/button').click()
 
-#cadastrando o CNPJ da Unidade
-navegador.find_element('xpath','//*[@id="cnpj"]').send_keys('42.054.740/0001-50')
-navegador.find_element('xpath','/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button[2]').click()
-time.sleep(2)
 
-#Modal de cadastro da Unidade
-navegador.find_element('css selector', '#name').send_keys('13765-MULTIPLA-ER 24 DE OUTUBRO2')
-navegador.find_element('xpath','//*[@id="account"]').send_keys('13765-MULTIPLA2')
-time.sleep(2)
-navegador.find_element('css selector', 'button[form="company-form"]').click()
-time.sleep(5)
+#navegador.find_element('xpath','//*[@id="root"]/div/main/div/div/div/div[1]/div[1]/button').click()
+
+for index,row in df.iterrows():
+    #pagina de cadastro
+    navegador.get('https://gruponatureza.trilogo.app/companies')
+    time.sleep(2)
+    #Nova Unidade
+    navegador.find_element('xpath','//*[@id="root"]/div/main/div/div/div/div[1]/div[1]/button').click()
+    print(' \n Linha: ' + str(index) + " Nome da filial é  " + row["Nome da Filial"]+"\n")
+    #cadastrando o CNPJ da Unidade
+    navegador.find_element('xpath', '//*[@id="cnpj"]').send_keys(row['CNPJ'])
+    time.sleep(2)
+    navegador.find_element('xpath','/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button[2]').click()
+    time.sleep(2)
+   
+    #Modal de cadastro da Unidade
+    navegador.find_element('css selector', '#name').send_keys(row['Nome da Filial'])
+    navegador.find_element('xpath','//*[@id="account"]').send_keys(row['Fantasia'])
+    time.sleep(2)
+    navegador.find_element('css selector', 'button[form="company-form"]').click()
+    time.sleep(2)
+    print('Próxima empresa 🤖')
 
 
 #ferramenta de estração de dados das páginas, p/ pegar os usuarios
