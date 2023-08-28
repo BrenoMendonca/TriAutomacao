9bb3bb59ab636897ea6c481848c54130ffd112cf
Breno Mendonca <breno_a99@hotmail.com>
2023-08-28 17:22:05 -0300

    Corrgindo possiveis erros com Try exeption

--- teste.py
+++ teste.py
@@ -24,45 +24,57 @@ navegador = webdriver.Chrome(service=servico)
 
 
 #Acessando o dominio
-navegador.get('https://gruponatureza.trilogo.app')
+navegador.get('https://homologacao.trilogo.app')
 
 time.sleep(2)
 
 #Solicitando os dados do usuario
-login = input("Digite seu email Trilogo: ")
-senha = input("Digite sua senha: ")
+#login = input("Digite seu email Trilogo: ")
+#senha = input("Digite sua senha: ")
 #Inserindo dados de login
-navegador.find_element('xpath','//*[@id="email"]').send_keys(login)
-navegador.find_element('xpath','//*[@id="password"]').send_keys(senha)
+
+#TESTE
+navegador.find_element('xpath','//*[@id="email"]').send_keys('breno.mendonca@trilogo.com.br')
+navegador.find_element('xpath','//*[@id="password"]').send_keys('@Breno059')
+
+#navegador.find_element('xpath','//*[@id="email"]').send_keys(login)
+#navegador.find_element('xpath','//*[@id="password"]').send_keys(senha)
 navegador.find_element('xpath','//*[@id="submit"]').click()
 
 time.sleep(3)  # Aguarda 03 segundos (pode ajustar esse valor)
 
 
-
-
-#navegador.find_element('xpath','//*[@id="root"]/div/main/div/div/div/div[1]/div[1]/button').click()
-
 for index,row in df.iterrows():
     #pagina de cadastro
-    navegador.get('https://gruponatureza.trilogo.app/companies')
+    navegador.get('https://homologacao.trilogo.app/companies')
     time.sleep(2)
     #Nova Unidade
-    navegador.find_element('xpath','//*[@id="root"]/div/main/div/div/div/div[1]/div[1]/button').click()
+    navegador.find_element('xpath','//*[@id="rc-tabs-0-panel-1"]/div/div/div/div[1]/div[1]/button').click()
     print(' \n Linha: ' + str(index) + " Nome da filial é  " + row["Nome da Filial"]+"\n")
+
     #cadastrando o CNPJ da Unidade
+    
     navegador.find_element('xpath', '//*[@id="cnpj"]').send_keys(row['CNPJ'])
     time.sleep(2)
+    
     navegador.find_element('xpath','/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button[2]').click()
     time.sleep(2)
    
-    #Modal de cadastro da Unidade
-    navegador.find_element('css selector', '#name').send_keys(row['Nome da Filial'])
-    navegador.find_element('xpath','//*[@id="account"]').send_keys(row['Fantasia'])
-    time.sleep(2)
-    navegador.find_element('css selector', 'button[form="company-form"]').click()
-    time.sleep(2)
-    print('Próxima empresa 🤖')
+    try: 
+        #Modal de cadastro da Unidade
+        navegador.find_element('css selector', '#name').send_keys(row['Nome da Filial'])
+        navegador.find_element('xpath','//*[@id="account"]').send_keys(row['Fantasia'])
+        time.sleep(2)
+        navegador.find_element('css selector', 'button[form="company-form"]').click()
+        time.sleep(2)
+        print('Próxima empresa 🤖')
+    except:
+        print("CNPJ INVALIDO ou já cadastrado " + row['CNPJ'])
+
+        time.sleep(3)
+        continue  
+
+print(" \n Fim do Programa! \n")
 
 
 #ferramenta de estração de dados das páginas, p/ pegar os usuarios
