cd40b280bc5879ef6e8e699b730067840666b0e0
Breno Mendonca <breno_a99@hotmail.com>
2023-09-13 18:27:45 -0300

    Criando o Array para guardar as lojas que estão dando erro

--- CadastroCNPJ/teste.py
+++ CadastroCNPJ/teste.py
@@ -25,7 +25,7 @@ navegador = webdriver.Chrome(service=servico)
 
 
     #Acessando o dominio
-navegador.get('https://homologacao.trilogo.app')
+navegador.get('https://smartfitnovo.trilogo.app/companies')
 
 
 time.sleep(2)
@@ -43,38 +43,46 @@ navegador.find_element('xpath','//*[@id="submit"]').click()
 
 time.sleep(3)  # Aguarda 03 segundos (pode ajustar esse valor)
 
-
+Filiais_com_erro = [] #Criando o Array 
 for index,row in df.iterrows():
         #pagina de cadastro
-        navegador.get('https://homologacao.trilogo.app/companies')
+        navegador.get('https://smartfitnovo.trilogo.app/companies')
         time.sleep(2)
         #Nova Unidade
-        navegador.find_element('xpath','//*[@id="rc-tabs-0-panel-1"]/div/div/div/div[1]/div[1]/button').click()
+        navegador.find_element('xpath','//*[@id="root"]/div/main/div/div/div/div[1]/div[1]/button').click()
         print(' \n Linha: ' + str(index) + " Nome da filial é  " + row["Nome da Filial"]+"\n")
 
         #cadastrando o CNPJ da Unidade
+        try:
+            navegador.find_element('xpath', '//*[@id="cnpj"]').send_keys(row['CNPJ'])
+            time.sleep(2)
         
-        navegador.find_element('xpath', '//*[@id="cnpj"]').send_keys(row['CNPJ'])
-        time.sleep(2)
-        
-        navegador.find_element('xpath','/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button[2]').click()
-        time.sleep(2)
-    
+            navegador.find_element('xpath','/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button[2]').click()
+            time.sleep(2)
+        except:
+             print("CNPJ invalido"+ row['CNPJ'])
+             Filiais_com_erro.append(row)  # Adicionando a filial com erro ao Array
         try: 
             #Modal de cadastro da Unidade
             navegador.find_element('css selector', '#name').send_keys(row['Nome da Filial'])
             navegador.find_element('xpath','//*[@id="account"]').send_keys(row['Fantasia'])
             time.sleep(2)
-            navegador.find_element('css selector', 'button[form="company-form"]').click()
+            #navegador.find_element('css selector', 'button[form="company-form"]').click()
             time.sleep(2)
             print('Próxima empresa 🤖')
         except:
-            print("CNPJ INVALIDO ou já cadastrado " + row['CNPJ'])
-
+            print("CNPJ já cadastrado " + row['CNPJ'])
+            Filiais_com_erro.append(row)  # Adicionando a filial com erro ao Array
             time.sleep(3)
             continue  
 
-print(" \n Fim do Programa! \n")
+#print(" \n Fim do Programa! \n")
+if Filiais_com_erro:
+    print("Lojas com erro:")
+    for loja in Filiais_com_erro:
+        print(f"CNPJ: {loja['CNPJ']}, Nome da Filial: {loja['Nome da Filial']}")
+else:
+    print("Todas as lojas foram cadastradas com sucesso.")
 
 
 #ferramenta de estração de dados das páginas, p/ pegar os usuarios
